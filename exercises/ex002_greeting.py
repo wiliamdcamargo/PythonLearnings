2a55889e17c625d2a01465d250264b878ac74bbb
Wiliam Camargo <camargo.mailiw@gmail.com>
2023-10-25 11:57:23 -0400

    Rewrite the exercices 1, 2 and 3.

---
+++ exercises/ex002_greeting.py
@@ -0,0 +1,3 @@
+# Write a program that reads a person's name and displays a welcome message.
+name = input("What is your name? ")
+print("Nice to meet you, {}!".format(name))
---
+++ exercises/ex003_sum_two_numbers.py
@@ -0,0 +1,5 @@
+# Create a program that reads two numbers and displays the sum between them.
+number_one = int(input("Enter the first number: "))
+number_two = int(input("Enter the second number: "))
+sum = number_one + number_two
+print("The sum of the number {} and {} is equal to {}!".format(number_one, number_two, sum))
