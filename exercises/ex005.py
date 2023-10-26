2af1f9021bce91c49e6312edaed3f0639ff140e3
Wiliam Camargo <camargo.mailiw@gmail.com>
2023-10-26 19:26:20 -0400

    Exercise 4 was rewrite.

--- exercises/ex005.py
+++
@@ -1,3 +0,0 @@
-# Create a program that reads an integer number and displays its successor and predecessor on the screen.
-number = int(input('Enter an integer number: '))
-print('Analyzing the value {}. Its predecessor is {} and successor is {}.'.format(number, number - 1, number + 1))
