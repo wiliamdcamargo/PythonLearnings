6687d10318c1f83acc47576c0bf5f677cc211281
Wiliam Camargo <camargo.mailiw@gmail.com>
2023-10-26 19:32:42 -0400

    Exercise 6 was rewrite

--- exercises/ex006.py
+++
@@ -1,5 +0,0 @@
-# Create an algorithm that reads a number and displays its double, triple and, squere root on the screen.
-number = int(input('Enter a number: '))
-print('The double of {} is equal to {}.'.format(number, number * 2))
-print('The triple of {} is equal to {}.'.format(number, number * 3))
-print('The square root of {} is equal to {:.2f}.'.format(number, number ** 0.5))
